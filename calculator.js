2bf502b9ee91dd826ef18cb1151ddaa65e5f1ef0
Joshua Gray <graydog.8719@gmail.com>
2024-04-25 16:12:18 -0600

    created functions with my paired programming partner, Rafael

--- calculator.js
+++ calculator.js
@@ -1,4 +1,70 @@
-function calculate(expression) {}
+// define operator functions
+function add(x, y) {
+  return x + y
+}
+
+function subtract(x, y) {
+  return x - y
+}
+
+function multiply(x, y) {
+  return x * y
+}
+
+function divide(x, y) {
+  return x / y
+}
+
+function power(x, y) {
+  return x ^ y
+}
+
+function mod(x, y) {
+  x % y
+}
+
+function squareRoot(x) {
+  return Math.sqrt(x)
+}
+
+function calculate(expression) {
+  expression = expression.split(' ')
+
+  let num1
+  let operator
+  let num2
+
+  if (expression.length === 3) {
+    num1 = Number(expression[0])
+    operator = expression[1]
+    num2 = Number(expression[2])
+  } else if (expression.length === 2) {
+    operator = expression[0]
+    num1 = Number(expression[1])
+  } else {
+    return alert("Too many / Too Few Inputs")
+  }
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    return alert("That's not a number, sillypants!")
+  }
+
+  if (operator === '+') {
+    return num1 + num2
+  } else if (operator === '-') {
+    return num1 - num2
+  } else if (operator === '*') {
+    return num1 * num2
+  } else if (operator === '^') {
+    return num1 ** num2
+  } else if (operator === '%') {
+    return num1 % num2
+  } else if (operator === '/') {
+    return num1 / num2
+  } else if (operator === 'sqrt') {
+    return Math.sqrt(num1)
+  }
+}
 
 /* **************** DO NOT EDIT THE CODE BELOW **************** */
 /* ************************************************************ */
